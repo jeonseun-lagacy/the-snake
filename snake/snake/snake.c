486f1992695c8d4c8c7f72339c84542a5d2451cb
jeonseun <wjstpdns2015@naver.com>
2020-02-16 23:58:14 +0900

    * 뱀 길이 증가, 맵 이탈 확인 구현
    - 뱀 길이증가 함수, 맵 이탈 확인 함수 구현
    - 아이템 획득시 길이 증가 하도록 아이템 관련 함수 추가 필요

--- snake/snake/snake.c
+++ snake/snake/snake.c
@@ -8,6 +8,10 @@
 //뱀 모양
 #define SNAKE_SHAPE "○"
 
+//뱀 초기위치 (쉬운 좌표계산을 위해 맵 좌표와 동일한 짝수로 생성)
+#define SNAKE_POS_X 6
+#define SNAKE_POS_Y 4
+
 #define UP 72
 #define DOWN 80
 #define LEFT 75
@@ -25,11 +29,23 @@ void turnSnake(int nKey, struct Snake* snake);
 //진행방향으로 뱀 이동
 void moveSnake(struct Snake* snake);
 
+//뱀의 맵 이탈 확인
+int isSnakeOut(const struct Snake snake, int mapWidth, int mapHeight);
+
 //뱀의 길이 확장
 int addSnakeLength(struct Snake* snake);
 
 //뱀의 길이와 좌표를 이용하여 화면에 출력
 void drawSnake(const struct Snake snake);
+
+//이전에 그려진 뱀을 지움
+void eraseSnake(const struct Snake snake);
+
+//뱀의 먹이 생성
+void spawnItem(struct Item* item, int mapWidth, int mapHeight);
+
+//맵 그리기
+void drawMap(int width, int height);
 
 //뱀 방향, 길이, 좌표 저장 구조체
 struct Snake {
@@ -38,18 +54,42 @@ struct Snake {
 	POINT pos[MAX_SNAKE_LENGTH];
 };
 
+//아이템 모양 및 좌표 
+//TODO 아이템 종류 추가 가능하도록 만들기
+struct Item {
+	char* itemShape;
+	int state;//생성되었으면 1, 제거되었으면 0
+	POINT pos;
+};
+
 int main(void) {
-	hideCursor();
-	const int game_speed = 66;
+	//맵 최대 크기
+	int mapWidth = 20;
+	int mapHeight = 20;
+	
+	int gameSpeed = 66;
 	int nKey;
 	clock_t CurTime, OldTime;
 
 	//게임 시작 전 뱀 상태 초기화
-	struct Snake snake = { 77, 1, {5, 1} };
+	struct Snake snake = { 77, 1, {SNAKE_POS_X, SNAKE_POS_Y} };
 
+	//커서 가리기
+	hideCursor();
+
+	//랜덤 시드 초기화
+	srand(time(NULL));
+	//한 스테이지에 대한 맵 생성 및 게임 수행
+	drawMap(mapWidth, mapHeight);
 	while (1) {
-		system("cls");
-		//gotoxy(snake.pos->x, snake.pos->y);
+		//뱀의 맵 이탈 확인
+		if (isSnakeOut(snake, mapWidth, mapHeight)) {
+			break;
+		}
+		//뱀 최대길이 달성
+		if (snake.length == MAX_SNAKE_LENGTH) {
+			break;
+		}
 		OldTime = clock();
 		if (_kbhit()) {
 			nKey = _getch();
@@ -58,20 +98,24 @@ int main(void) {
 				nKey = _getch();
 				//키 입력값으로 뱀 회전
 				turnSnake(nKey, &snake);
+				//TEST...
+				//addSnakeLength(&snake);
 			}
 		}
-		//뱀 방향에 맞게 이동
-		moveSnake(&snake);
+
 		drawSnake(snake);
-		
+
 		//뱀 진행 속도 조절
 		while (1) {
 			CurTime = clock();
-			if (CurTime - OldTime > game_speed) {
+			if (CurTime - OldTime > gameSpeed) {
 				break;
 			}
 		}
+		eraseSnake(snake);
+		moveSnake(&snake);
 	}
+	getchar();
 	return 0;
 }
 
@@ -122,31 +166,81 @@ void moveSnake(struct Snake* snake){
 		break;
 	}
 	
-	//머리 이외의 부분이 있는 경우
+	//길이가 2이상인 경우
 	if (snake->length > 1) {
-		for (int i = 1; i < snake->length; i++) {
-			
+		for (int i = snake->length - 1; i > 0; i--) {
+			snake->pos[i].x = snake->pos[i-1].x;
+			snake->pos[i].y = snake->pos[i-1].y;
 		}
 	}
 	snake->pos[0].x += deltaX;
 	snake->pos[0].y += deltaY;
 }
 
+int isSnakeOut(const struct Snake snake, int mapWidth, int mapHeight){
+	if (!snake.pos->x || !snake.pos->y) {
+		return 1;
+	}
+	else if (snake.pos->x == (mapWidth*2 -2) || snake.pos->y == mapHeight - 1) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
 int addSnakeLength(struct Snake* snake){
 	//길이 증가 가능한 경우 증가시키고 1 반환
 	if (snake->length < MAX_SNAKE_LENGTH) {
 		//TODO 머리 부분에 새로운 몸통을 추가하고 나머지를 한칸씩 밀어냄
+		snake->length++;
+		for (int i = snake->length - 1; i > 0; i--) {
+			snake->pos[i].x = snake->pos[i - 1].x;
+			snake->pos[i].y = snake->pos[i - 1].y;
+		}
+		return 1;
 	}
 	//최대 길이인 경우 0반환
 	else {
 		return 0;
 	}
-	
 }
 
 void drawSnake(const struct Snake snake){
 	for (int i = 0; i < snake.length; i++) {
 		gotoxy(snake.pos[i].x, snake.pos[i].y);
 		printf("%s", SNAKE_SHAPE);
+	}
+}
+
+void eraseSnake(const struct Snake snake){
+	for (int i = 0; i < snake.length; i++) {
+		gotoxy(snake.pos[i].x, snake.pos[i].y);
+		for (int j = 0; j < strlen(SNAKE_SHAPE); j++) {
+			printf(" ");
+		}
+	}
+}
+
+//TODO 맵 테두리 내부에 생성되도록 만들것
+void spawnItem(struct Item* item, int mapWidth, int mapHeight){
+	int randX, randY;//생성될 아이템 좌표
+	randX = rand() % mapWidth;
+	randY = rand() % mapHeight;
+	item->pos.x = randX;
+	item->pos.y = randY;
+	gotoxy(randX, randY);
+}
+
+void drawMap(int width, int height){
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if (i == 0 || j == width - 1 || i == height - 1 || j == 0) {
+				printf("▒");
+			}
+			else {
+				printf("  ");
+			}
+		}printf("\n");
 	}
 }
